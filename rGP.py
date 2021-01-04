214038dac690bfc5ca7836a526fe32c8b3ca6ec0
alierenaud <30876340+alierenaud@users.noreply.github.com>
2021-01-04 12:47:01 -0500

    Update rGP.py

--- rGP.py
+++ rGP.py
@@ -52,11 +52,42 @@ class GP:
         L = np.linalg.cholesky(Sigma)
         return(np.matmul(L,Z)+self.meanVec(loc))
     
+    def rCondGP(self, locPred, locObs, valObs):
+        loc = np.concatenate((locPred, locObs))
+        Sigma = self.covMatrix(loc)
+        mu = self.meanVec(loc)
+        
+        nlocPred = locPred.shape[0]
+        nlocObs = locObs.shape[0]
+        nloc = nlocPred + nlocObs
+        
+        mu1 = mu[0:nlocPred]
+        mu2 = mu[nlocPred:nloc]
+        
+        Sigma11 = Sigma[0:nlocPred, 0:nlocPred]
+        Sigma12 = Sigma[0:nlocPred, nlocPred:nloc]
+        Sigma21 = np.transpose(Sigma12)
+        Sigma22 = Sigma[nlocPred:nloc, nlocPred:nloc]
+        
+        invSigma22 = np.linalg.inv(Sigma22)
+        
+        mu1c2 = mu1 + np.matmul(np.matmul(Sigma12,invSigma22),  valObs-mu2)
+        Sigma1c2 = Sigma11 - np.matmul(np.matmul(Sigma12,invSigma22),  Sigma21)
+        
+        L = np.linalg.cholesky(Sigma1c2)
+        Z = random.normal(size=(nlocPred,1))
+        return(np.matmul(L,Z)+mu1c2)
+    
     
 def gaussianCov(sigma2,l):
     def evalCov(x,y):
         return(sigma2*np.exp(-np.linalg.norm(x-y)/2/l**2))
     return(evalCov)  
+
+
+### usage examples
+
+### zero mean
 
 def zeroMean(x):
     return(0)    
@@ -82,7 +113,7 @@ def makeGrid(xlim,ylim,res):
     return(grid)
 
 
-res = 100
+res = 50
 gridLoc = makeGrid([0,1], [0,1], res)
 
 
@@ -113,8 +144,44 @@ plt.ylim(0,1)
 
 plt.show()
 
+### zero mean (cond)
+
+nlocObs = 500
+nloc = res**2
+
+locObs = gridLoc[0:nlocObs,:]
+locPred = gridLoc[nlocObs:nloc,:]
+
+
+valObs = newGP.rGP(gridLoc[0:nlocObs,:])
+
+resCondGP = newGP.rCondGP(locPred, locObs, valObs)
+
+resGP = np.concatenate((valObs, resCondGP))
+
+#### to make plot ####
+
+imGP = resGP.reshape(res,res)
+
+x = np.linspace(0,1, res+1) 
+y = np.linspace(0,1, res+1) 
+X, Y = np.meshgrid(x,y) 
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_aspect('equal')
+
+plt.pcolormesh(X,Y,imGP)
+
+plt.xlim(0,1)
+plt.ylim(0,1)
+
+
+plt.show()
+
 #### ####
 
+### cross mean
 
 def fct(x):
     return(np.exp(-np.minimum((x[0]-0.5)**2,(x[1]-0.5)**2)/0.003))
@@ -143,8 +210,44 @@ plt.ylim(0,1)
 
 plt.show()
 
+### cross mean (cond)
+
+nlocObs = 500
+nloc = res**2
+
+locObs = gridLoc[0:nlocObs,:]
+locPred = gridLoc[nlocObs:nloc,:]
+
+
+valObs = newGP.rGP(gridLoc[0:nlocObs,:])
+
+resCondGP = newGP.rCondGP(locPred, locObs, valObs)
+
+resGP = np.concatenate((valObs, resCondGP))
+
+#### to make plot ####
+
+imGP = resGP.reshape(res,res)
+
+x = np.linspace(0,1, res+1) 
+y = np.linspace(0,1, res+1) 
+X, Y = np.meshgrid(x,y) 
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_aspect('equal')
+
+plt.pcolormesh(X,Y,imGP)
+
+plt.xlim(0,1)
+plt.ylim(0,1)
+
+
+plt.show()
+
 #### ####
 
+### circle mean
 
 def fct(x):
     return(np.exp(-(0.25-np.sqrt((x[0]-0.5)**2+(x[1]-0.5)**2))**2/0.003))
@@ -152,6 +255,41 @@ def fct(x):
 newGP = GP(fct,gaussianCov(1,1))
 
 resGP = newGP.rGP(gridLoc)
+
+#### to make plot ####
+
+imGP = resGP.reshape(res,res)
+
+x = np.linspace(0,1, res+1) 
+y = np.linspace(0,1, res+1) 
+X, Y = np.meshgrid(x,y) 
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_aspect('equal')
+
+plt.pcolormesh(X,Y,imGP)
+
+plt.xlim(0,1)
+plt.ylim(0,1)
+
+
+plt.show()
+
+### circle mean (cond)
+
+nlocObs = 500
+nloc = res**2
+
+locObs = gridLoc[0:nlocObs,:]
+locPred = gridLoc[nlocObs:nloc,:]
+
+
+valObs = newGP.rGP(gridLoc[0:nlocObs,:])
+
+resCondGP = newGP.rCondGP(locPred, locObs, valObs)
+
+resGP = np.concatenate((valObs, resCondGP))
 
 #### to make plot ####
 
